addd60f098293de8e29c08813306bb2de0cc6986
Charles <cescapecs@gmail.com>
2021-04-21 13:13:02 +1200

    rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("redData version 0.0.34 (2021-04-03 14:24:34)")
+    packageStartupMessage("redData version 0.0.50 (2021-04-21 12:21:30)")
 }
